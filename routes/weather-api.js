c62f97071f9a11dfaecd58002ae97e9c553ee70a
Henko Meyer <84342604+Henkomeyer@users.noreply.github.com>
2023-05-29 11:08:39 +0200

    Upload of initial express server

---
+++ routes/weather-api.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const router = express.Router();
+const fetch = require('node-fetch');
+
+const API_KEY = 'f931009061a9a46ff152fc8188a50028'; 
+
+router.post('/', async (req, res) => {
+  const { zipcode } = req.body;
+
+  try {
+    const response = await fetch(
+      `https://api.openweathermap.org/data/2.5/weather?zip=${zipcode}&units=metric&appid=${API_KEY}`
+    );
+    if (!response.ok) {
+      throw new Error('Failed to fetch weather data');
+    }
+    const data = await response.json();
+
+    const weatherData = {
+      temperature: data.main.temp,
+      description: data.weather[0].description,
+    };
+
+    res.json(weatherData);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Failed to retrieve weather data' });
+  }
+});
+
+module.exports = router;
