10c9effc56f12693156199eadeaab8d4882db694
Henko Meyer <84342604+Henkomeyer@users.noreply.github.com>
2023-06-03 17:53:45 +0200

    Attempt to use Zip code validation

--- routes/weather-api.js
+++ routes/weather-api.js
@@ -3,15 +3,18 @@ const router = express.Router();
 const fetch = require('node-fetch');
 
 const API_KEY = 'f931009061a9a46ff152fc8188a50028'; 
+const ZAPI_KEY = 'd9bbec05b341cf54f65ad181b39af0b7';
+
+let data1;
+
 
 router.get('/:location', async (req, res) => {
   let [lon, lat] = req.params.location.toString().split('_');
   console.log("Params = "+lon+", "+lat)
   try {
     const response = await fetch(
-      `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}`
+      `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=${API_KEY}`
     );
-    console.log(response)
     console.log('Fetching Weather Data')
     if (!response.ok) {
       throw new Error('Failed to fetch weather data');
@@ -22,6 +25,7 @@ router.get('/:location', async (req, res) => {
     console.error(error);
     res.status(500).json({ error: 'Failed to retrieve weather data' });
   }
+  
 });
 
 module.exports = router;
