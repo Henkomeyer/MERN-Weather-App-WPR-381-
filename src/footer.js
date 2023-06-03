d9216f52a4ccd7a00c7ebb436f1279c77cb9367a
Jaco <111895986+timejunky1@users.noreply.github.com>
2023-06-03 22:15:46 +0200

    Footer reg gemaak

--- src/footer.js
+++ src/footer.js
@@ -1,38 +1,42 @@
 
 function Footer({data}) {
-    switch (data.description) 
+    let description = "supermoon"
+    // if(data.description){
+    //     description = data.description
+    // }
+
+    switch (description)
     {
         case 'sunny':
-            return
+            return(
         (<footer>
         <h1>Remember to wear sunscreen.</h1>
-        </footer>)
+        </footer>))
         case 'stormy':
-            return
-        (<footer>
+            return(
+        (<footer>   
         <h1>Take an umbrella!</h1>
-        </footer>)
+        </footer>))
         case 'cloudy':
-            return
+            return(
         (<footer>
         <h1>Perhaps take a jacket.</h1>
-        </footer>)
+        </footer>))
         case 'snowy':
-            return
+            return(
         (<footer>
         <h1>Wear something warm.</h1>
-        </footer>)
-        case 'superoon': 
-        return
+        </footer>))
+        case 'supermoon': 
+        return(
         (<footer>
         <h1>Be on the lookout for wearwolves!</h1>
-        </footer>)
+        </footer>))
         default: 
-        return
+        return(
         (<footer>
             <h1>Take a look at the weather.</h1>
-        </footer>)
-    
+        </footer>))
     };
 }
 
